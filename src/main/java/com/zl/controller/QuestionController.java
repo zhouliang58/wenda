820615ebcf148593f5bebf1b0a157c30930105b8
liangduoduo666666 <798277403@qq.com>
2016-10-18 21:54:17 +0800

    first commit!

---
+++ src/main/java/com/zl/controller/QuestionController.java
@@ -0,0 +1,7 @@
+package com.zl.controller;
+
+/**
+ * Created by zhouliang on 2016/10/17.
+ */
+public class QuestionController {
+}
---
+++ src/main/java/com/zl/service/SensitiveService.java
@@ -0,0 +1,7 @@
+package com.zl.service;
+
+/**
+ * Created by zhouliang on 2016/10/17.
+ */
+public interface SensitiveService {
+}
